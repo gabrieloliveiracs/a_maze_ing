5b6772e402b62003ee01fcbb424f41c42249b786
gabrielo <gabrielo@student.42sp.org.br>
2026-09-24 13:11:16 -0300

    add ansi code helper funcs controller.py

--- controller.py
+++ controller.py
@@ -13,26 +13,44 @@ class MazeController:
     def _clear_screen(self) -> None:
         print("\033[2J\033[H", end="")
 
+    def _hide_cursor(self) -> None:
+        print("\033[?25l", end="", flush=True)
+
+    def _show_cursor(self) -> None:
+        print("\033[?25h", end="", flush=True)
+
+    def _move_cursor_top(self) -> None:
+        print("\033[H\n", end="")
+
     def process_maze(self, maze: MazeGenerator) -> None:
         self.renderer.grid = maze.grid
 
+        self._animate_generation(maze)
+        self._reset_animation_state()
+        self._solve_and_attach_path(maze)
+
+    def _animate_generation(self, maze: MazeGenerator) -> None:
         self._clear_screen()
-        print("\033[?25l", end="", flush=True)
+        self._hide_cursor()
+
         try:
-            for pos in maze.carve_path():
+            for current_position in maze.carve_path():
                 self.renderer.visited = maze.visited
-                self.renderer.current_cell = pos
+                self.renderer.current_cell = current_position
                 self.renderer.stack = maze.stack
-                print("\033[H\n", end="")
+
+                self._move_cursor_top()
                 self.renderer.render()
                 time.sleep(0.03)
         finally:
-            print("\033[?25h", end="", flush=True)
+            self._show_cursor()
 
+    def _reset_animation_state(self) -> None:
         self.renderer.visited = None
         self.renderer.current_cell = None
         self.renderer.stack = None
 
+    def _solve_and_attach_path(self, maze: MazeGenerator) -> None:
         solver = MazeSolver(maze)
         solver.dead_end_fill(maze)
 
@@ -41,6 +59,7 @@ class MazeController:
 
     def run(self, initial_maze: MazeGenerator) -> None:
         self.process_maze(initial_maze)
+
         try:
             while True:
                 self._clear_screen()
@@ -82,5 +101,5 @@ class MazeController:
                     input("Pressione ENTER para continuar...")
 
         except KeyboardInterrupt:
-            # handle Ctrl+C exit and restore cursor
-            print("\033[?25h\n\nSaindo...")
+            self._show_cursor()
+            print("\n\nSaindo...")
